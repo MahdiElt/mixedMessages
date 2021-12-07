6cab969e15f87a334965e94d4cad911f99d21b86
Elmahdi Eltayeb <eeltayeb95@gmail.com>
2021-12-07 10:54:13 -0500

    Add more Races, Classes and Weapons

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const console = require("node:console");
 const charGenerator = () => 
     {const charInfo = {                                            // Creating object that has all the components that will be generated into messsage
-        charRace: ['Elf', 'Dwarf', 'Human', 'Gnome', 'Khajit', 'Dranei', 'Satyr' ],
-        charClass: ['Mage', 'Warrior', 'Rogue', 'Hunter'],
-        charWeapon: ['Sword', 'Great Hammer', 'Battleaxe', 'Bow and Arrow', 'Spear']
+        charRace: ['Elf', 'Dwarf', 'Human', 'Gnome', 'Khajit', 'Dranei', 'Satyr', 'Werewolf' ],
+        charClass: ['Mage', 'Warrior', 'Rogue', 'Hunter', 'Fighter', 'Warlock', 'Druid'],
+        charWeapon: ['Sword', 'Great Hammer', 'Battleaxe', 'Bow and Arrow', 'Spear', 'Unarmed']
     }
 
     let createdChar = []; // Array to hold generated message
@@ -18,4 +18,7 @@ const charGenerator = () =>
 
     return console.log( ' ' + `Your Race is: ${createdChar[0]} \n\ Your Class is: ${createdChar[1]} \n\ Your Weapon is: ${createdChar[2]}`)}
 // format and log generated message
-    charGenerator()
+
+charGenerator()
+charGenerator()
+charGenerator()
