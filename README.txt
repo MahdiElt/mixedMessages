9e0d562ce466bac5de0746e6dd82bcc7a060fa86
Elmahdi Eltayeb <eeltayeb95@gmail.com>
2021-12-07 10:33:52 -0500

    Add fucntion and log message

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is a function that will generate a random Dnd style character and print the statement to the console.
---
+++ script.js
@@ -0,0 +1,21 @@
+const console = require("node:console");
+const charGenerator = () => 
+    {const charInfo = {                                            // Creating object that has all the components that will be generated into messsage
+        charRace: ['Elf', 'Dwarf', 'Human', 'Gnome', 'Khajit', 'Dranei', 'Satyr' ],
+        charClass: ['Mage', 'Warrior', 'Rogue', 'Hunter'],
+        charWeapon: ['Sword', 'Great Hammer', 'Battleaxe', 'Bow and Arrow', 'Spear']
+    }
+
+    let createdChar = []; // Array to hold generated message
+
+    const randNum = n => {           // Will use this function to randomly select components for generated message
+        return Math.floor(Math.random()*n)    
+        }
+
+    for (let info in charInfo ) {
+        createdChar.push(charInfo[info][randNum(charInfo[info].length)])   // add in each randomly selected property to empty array
+    };
+
+    return console.log( ' ' + `Your Race is: ${createdChar[0]} \n\ Your Class is: ${createdChar[1]} \n\ Your Weapon is: ${createdChar[2]}`)}
+// format and log generated message
+    charGenerator()
